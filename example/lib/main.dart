8bea1646c8bd0b0fee56cb9863148d88efa1e805
Sandwich2080 <androidsandwich@163.com>
2020-04-02 18:36:59 +0800

    [modified] Change some configurations

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -43,50 +43,57 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Save image to gallery"),
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              RepaintBoundary(
-                key: _globalKey,
-                child: Container(
-                  width: 200,
-                  height: 200,
-                  color: Colors.red,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 15),
-                child: RaisedButton(
-                  onPressed: _saveScreen,
-                  child: Text("保存页面图片到相册"),
-                ),
-                width: 200,
-                height: 44,
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 15),
-                child: RaisedButton(
-                  onPressed: _getHttp,
-                  child: Text("保存网络图片到相册"),
-                ),
-                width: 200,
-                height: 44,
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 15),
-                child: RaisedButton(
-                  onPressed: _saveVideo,
-                  child: Text("保存网络视频到相册"),
-                ),
-                width: 200,
-                height: 44,
-              ),
-            ],
+      appBar: AppBar(
+        title: Text("Save image to gallery"),
+      ),
+      body: Container(
+        margin: EdgeInsets.only(top: 10),
+        child: _buildBody(),),
+    );
+  }
+
+  _buildBody() {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          RepaintBoundary(
+            key: _globalKey,
+            child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.red,
+            ),
           ),
-        ));
+          Container(
+            padding: EdgeInsets.only(top: 15),
+            child: RaisedButton(
+              onPressed: _saveScreen,
+              child: Text("保存页面图片到相册"),
+            ),
+            width: 200,
+            height: 44,
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 15),
+            child: RaisedButton(
+              onPressed: _saveNetworkImage,
+              child: Text("保存网络图片到相册"),
+            ),
+            width: 200,
+            height: 44,
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 15),
+            child: RaisedButton(
+              onPressed: _saveVideo,
+              child: Text("保存网络视频到相册"),
+            ),
+            width: 200,
+            height: 44,
+          ),
+        ],
+      ),
+    );
   }
 
   _saveScreen() async {
@@ -94,21 +101,26 @@ class _MyHomePageState extends State<MyHomePage> {
         _globalKey.currentContext.findRenderObject();
     ui.Image image = await boundary.toImage();
     ByteData byteData = await image.toByteData(format: ui.ImageByteFormat.png);
-    final result = await ImageGallerySaver.saveImage(byteData.buffer.asUint8List());
+    final result =
+        await ImageGallerySaver.saveImage(byteData.buffer.asUint8List());
     print(result);
   }
 
-  _getHttp() async {
-    var response = await Dio().get("https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=a62e824376d98d1069d40a31113eb807/838ba61ea8d3fd1fc9c7b6853a4e251f94ca5f46.jpg", options: Options(responseType: ResponseType.bytes));
-    final result = await ImageGallerySaver.saveImage(Uint8List.fromList(response.data));
+  _saveNetworkImage() async {
+    var response = await Dio().get(
+        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1585829049863&di=0fe537a3a3aa640cbeac2ec0a753e8c9&imgtype=0&src=http%3A%2F%2Ff.hiphotos.baidu.com%2Fbaike%2Fs%253D220%2Fsign%3De03c0bc3f3deb48fff69a6dcc01e3aef%2Fc83d70cf3bc79f3de51a7592baa1cd11738b299c.jpg",
+        options: Options(responseType: ResponseType.bytes));
+    final result =
+        await ImageGallerySaver.saveImage(Uint8List.fromList(response.data));
     print(result);
   }
 
- _saveVideo() async {
+  _saveVideo() async {
     var appDocDir = await getTemporaryDirectory();
     String savePath = appDocDir.path + "/temp.mp4";
-    await Dio().download("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4", savePath);
+    await Dio().download(
+        "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4", savePath);
     final result = await ImageGallerySaver.saveFile(savePath);
     print(result);
- }
+  }
 }
